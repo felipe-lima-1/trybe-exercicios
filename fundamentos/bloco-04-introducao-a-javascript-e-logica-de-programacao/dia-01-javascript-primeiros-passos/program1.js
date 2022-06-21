e62ac2f9123f61b145c536de7f6040917a064d3d
Felipe <luizbrutante@gmail.com>
2022-06-21 18:35:55 -0300

    Comeco dos exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/program1.js
@@ -0,0 +1,35 @@
+// const a = 5;
+// const b = 10;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+// const a = 10
+// const b = 20
+
+// if (a > b) {
+//     console.log('Maior é o', a);
+// }else {
+//     console.log('Maior é o', b);
+// }
+
+const a = 10;
+const b = 20;
+const c = 5;
+
+if (a > b) {
+    console.log('Maior é o', a);
+}else if (a > c) {
+    console.log('Maior é o', a);
+}else if (b > a) {
+    console.log('Maior é o', b);
+}else if (b > c) {
+    console.log('Maior é o', b);
+}
+else{
+    console.log('Maior é o', c);
+}  
