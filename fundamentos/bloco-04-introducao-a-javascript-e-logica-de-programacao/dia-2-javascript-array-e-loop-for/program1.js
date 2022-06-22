772332b8550f4fc69f25202a583e76d5ff8eb0fb
Felipe <luizbrutante@gmail.com>
2022-06-22 18:08:49 -0300

    Exercicios 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/program1.js
@@ -0,0 +1,27 @@
+// Exercicio 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// console.log(numbers);
+
+
+// Exercicio 2
+
+let soma = 0;
+for (index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+}
+console.log(soma);
+
+
+
+
+
+
+
+
+// Exercicio 
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
