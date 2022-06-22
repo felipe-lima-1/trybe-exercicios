ed5ab6d03d953eeb5ada336bde8ae972eae55caa
Felipe <luizbrutante@gmail.com>
2022-06-22 10:50:38 -0300

    exercicio 8

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/program8.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/program8.js
@@ -1,8 +1,8 @@
-const a = 13;
-const b = 17;
+const a = 12;
+const b = 15;
 const c = 31;
 
-if (a / 2 || b / 2 || c /2 % 0){
+if (a % 2 === 0 || b % 2 ===0 || c % 2 === 0){
     console.log(true);
 }else {
     console.log(false);
