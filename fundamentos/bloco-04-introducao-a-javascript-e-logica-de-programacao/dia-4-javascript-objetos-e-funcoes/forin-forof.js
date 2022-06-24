4777783485daae2afec4e561d77939de61692099
Felipe <luizbrutante@gmail.com>
2022-06-24 19:38:18 -0300

    intro e exercicios 1 ao 4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/forin-forof.js
@@ -0,0 +1,30 @@
+// let cars = ['Saab', 'Volvo', 'BMW'];
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index + ': ' + car[index]);
+//   }
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (let index in names) {
+    console.log('Ola ' + names[index]);
+  }
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+  
+  for (let key in car) {
+    console.log(key, car[key]);
+    }
