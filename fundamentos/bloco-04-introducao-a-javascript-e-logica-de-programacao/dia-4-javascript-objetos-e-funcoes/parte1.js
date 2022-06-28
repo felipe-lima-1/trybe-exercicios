d11f6571637c366a83955865259ef0b129d9a4db
Felipe <luizbrutante@gmail.com>
2022-06-28 13:14:42 -0300

    Exercicios parte 1 completos.1

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
@@ -52,7 +52,7 @@ let leitor = {
     },
   ],
 };
-console.log ('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+console.log ('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
 
 // 7 -
 
